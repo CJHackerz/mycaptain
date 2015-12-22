9f2cdbafc127b0db740e4dc6b4ec919a87852e3e
Abhishek Kalia <abhishekkalia@mail.com>
2015-12-22 17:39:18 +0530

    Style forum and contribution views

--- app/assets/javascripts/statics.js
+++ app/assets/javascripts/statics.js
@@ -11,7 +11,7 @@ $(function(){
         // You cannot use your own buttons. Sorry.
       effect: "fade",
         // [string] Can be either "slide" or "fade".
-      interval: 300,
+      interval: 700,
         // [number] Time spent on each slide in milliseconds.
       auto: true,
         // [boolean] Start playing the slideshow on load.
--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -4,6 +4,7 @@ class Workshop < ActiveRecord::Base
   has_many :users
   has_many :posts
   has_many :contributions
+  has_many :forum_threads
 
   has_attached_file :cover, styles: { medium: "800x300#", thumb: "300x300#" }, default_url: "missing_cover.png"
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
